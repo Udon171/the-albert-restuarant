fc53d725eaad4c1571a519308da89c8a21235974
Udon117 <dgkwells@outlook.com>
2025-12-15 22:47:24 +0000

    Setting up a new file for deployment configuration to Heroku.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn albert-restaurant.wsgi
--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aspy9e89!&%+k23h+rv_0^uti3lc#=oh)almc-1xit^hf^b95*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
